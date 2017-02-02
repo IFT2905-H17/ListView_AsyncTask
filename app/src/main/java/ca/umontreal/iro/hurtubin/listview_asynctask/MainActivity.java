3ae138915b908e8da82ee5a20c1003a3661c607d
Nicolas Hurtubise <316k@legtux.org>
2017-02-02 12:24:42 -0500

    Adds a random wikipedia article toast

--- app/src/main/java/ca/umontreal/iro/hurtubin/listview_asynctask/MainActivity.java
+++ app/src/main/java/ca/umontreal/iro/hurtubin/listview_asynctask/MainActivity.java
@@ -1,7 +1,14 @@
 package ca.umontreal.iro.hurtubin.listview_asynctask;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONException;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FetchArticle fetcher = new FetchArticle();
+
+        fetcher.execute("foo", "bar", "twado");
+    }
+
+    public class FetchArticle extends AsyncTask<String, Object, WikipediaArticle> {
+
+        @Override
+        protected WikipediaArticle doInBackground(String... params) {
+
+            Log.i("TEST", params[0]);
+
+            // Fetcher un article au hasard
+
+            WikipediaAPI api = new WikipediaAPI();
+
+            WikipediaArticle article = null;
+
+            try {
+                 article = api.random();
+
+            } catch (IOException|JSONException e) {
+                e.printStackTrace();
+            }
+
+            return article;
+        }
+
+        @Override
+        protected void onPostExecute(WikipediaArticle article) {
+            Toast.makeText(getApplicationContext(), article.title, Toast.LENGTH_LONG).show();
+        }
     }
 }
